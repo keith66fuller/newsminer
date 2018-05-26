97997ac71b8087e2fec259a29dbb32e0277af6a8
Jason O'Higgins <jason.ohiggins@gmail.com>
2018-05-26 11:23:57 -0500

    updated main page
    created main.html and built basic framework

--- routes/author-api-routes.js
+++ routes/author-api-routes.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
-    db.Author.findAll({
       include: [db.Post]
+    db.Author.findAll({
     }).then(function(dbAuthor) {
       res.json(dbAuthor);
     });
