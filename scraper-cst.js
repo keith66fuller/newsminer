5b5f480155cf3baea29df1c3250c06d2c3badfb5
Keith Fuller <keith66fuller@gmail.com>
2018-05-26 07:14:55 -0500

    have scrapers for 4 sites

---
+++ scraper-cst.js
@@ -0,0 +1,28 @@
+var seedUrl = 'https://chicago.suntimes.com/section/news/'
+var request = require("request")
+const cheerio = require('cheerio')
+var nodeScraper = require("node-scraper")
+var scraper = nodeScraper(seedUrl, {
+  selectors: ['.cst_article']
+});
+var scrape = require('html-metadata');
+scraper.scrape().on('done', function (err, statusCode, content) {
+  if (err) {
+    console.error(err);
+  } else {
+    content.forEach(item => {
+      (item.content).forEach(element => {
+        var c = cheerio.load(element.html)
+        var targetUrl = c('a').attr('href')
+        targetUrl = targetUrl.split('#')[0]
+        request(targetUrl, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            scrape(targetUrl, function (error, metadata) {
+              console.log(JSON.stringify(metadata, null, 2))
+            });
+          }
+        })
+      })
+    });
+  }
+});
--- server.js
+++ server.js
@@ -38,3 +38,5 @@ db.sequelize.sync({ force: true }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+
