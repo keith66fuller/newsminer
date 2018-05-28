bd34e86d4840f7965fc7e3ca80e37aec70e3d55b
Keith Fuller <keith66fuller@gmail.com>
2018-05-28 16:56:44 -0500

    Memorial Day

--- routes/article-api-routes.js
+++ routes/article-api-routes.js
@@ -12,66 +12,66 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the posts
-  app.get("/api/posts", function(req, res) {
+  // GET route for getting all of the articles
+  app.get("/api/articles", function(req, res) {
     var query = {};
-    if (req.query.author_id) {
-      query.AuthorId = req.query.author_id;
+    if (req.query.source_id) {
+      query.SourceId = req.query.source_id;
     }
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.Post.findAll({
+    // In this case, just db.Source
+    db.Article.findAll({
       where: query,
-      include: [db.Author]
-    }).then(function(dbPost) {
-      res.json(dbPost);
+      include: [db.Source]
+    }).then(function(dbArticle) {
+      res.json(dbArticle);
     });
   });
 
-  // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
+  // Get route for retrieving a single article
+  app.get("/api/articles/:id", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.Post.findOne({
+    // In this case, just db.Source
+    db.Article.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Author]
-    }).then(function(dbPost) {
-      res.json(dbPost);
+      include: [db.Source]
+    }).then(function(dbArticle) {
+      res.json(dbArticle);
     });
   });
 
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    db.Post.create(req.body).then(function(dbPost) {
-      res.json(dbPost);
+  // POST route for saving a new article
+  app.article("/api/articles", function(req, res) {
+    db.Article.create(req.body).then(function(dbArticle) {
+      res.json(dbArticle);
     });
   });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post.destroy({
+  // DELETE route for deleting articles
+  app.delete("/api/articles/:id", function(req, res) {
+    db.Article.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbPost) {
-      res.json(dbPost);
+    }).then(function(dbArticle) {
+      res.json(dbArticle);
     });
   });
 
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post.update(
+  // PUT route for updating articles
+  app.put("/api/articles", function(req, res) {
+    db.Article.update(
       req.body,
       {
         where: {
           id: req.body.id
         }
-      }).then(function(dbPost) {
-      res.json(dbPost);
+      }).then(function(dbArticle) {
+      res.json(dbArticle);
     });
   });
 };
