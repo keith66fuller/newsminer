374c60455495d9cb985ab35c09421e2349108364
Keith Fuller <keith66fuller@gmail.com>
2018-05-28 16:57:00 -0500

    M2

--- routes/source-api-routes.js
+++ routes/source-api-routes.js
@@ -1,44 +1,44 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/authors", function(req, res) {
+  app.get("/api/sources", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-      include: [db.Post]
-    db.Author.findAll({
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    // In this case, just db.Article
+      include: [db.Article]
+    db.Source.findAll({
+    }).then(function(dbSource) {
+      res.json(dbSource);
     });
   });
 
-  app.get("/api/authors/:id", function(req, res) {
+  app.get("/api/sources/:id", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Author.findOne({
+    // In this case, just db.Article
+    db.Source.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+      include: [db.Article]
+    }).then(function(dbSource) {
+      res.json(dbSource);
     });
   });
 
-  app.post("/api/authors", function(req, res) {
-    db.Author.create(req.body).then(function(dbAuthor) {
-      res.json(dbAuthor);
+  app.post("/api/sources", function(req, res) {
+    db.Source.create(req.body).then(function(dbSource) {
+      res.json(dbSource);
     });
   });
 
-  app.delete("/api/authors/:id", function(req, res) {
-    db.Author.destroy({
+  app.delete("/api/sources/:id", function(req, res) {
+    db.Source.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    }).then(function(dbSource) {
+      res.json(dbSource);
     });
   });
 
