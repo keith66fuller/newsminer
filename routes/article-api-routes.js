18b8a932a5df2c40311246ddc15a3c8d2b58133b
Keith Fuller <36208707+keith66fuller@users.noreply.github.com>
2018-06-03 21:53:26 -0500

    cleanup

--- routes/article-api-routes.js
+++ routes/article-api-routes.js
@@ -122,16 +122,13 @@ module.exports = function (app) {
 
     if (req.body.sources) {
       where.SourceId = req.body.sources
-      // where.SourceId = { [Op.or]: req.body.sources }
     }
 
     if (req.body.authors) {
       where.author = req.body.authors
-      // where.author = { [Op.or]: req.body.authors }
     }
 
     if (req.body.words) {
-      // where.title = { [Op.like]: '%'+req.body.words+'%' }
       where.title = { [Op.regexp]: '.+'+req.body.words+'.+' }
     }
 
