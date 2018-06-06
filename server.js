ded814702c0f2c6eb1707231c6ccbbc3242ab201
keith66fuller <keith66fuller@gmail.com>
2018-06-06 15:54:27 -0500

    corrected typo in server.js from last commit

--- server.js
+++ server.js
@@ -44,17 +44,12 @@ require("./routes/article-api-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
 require("./routes/source-routes.js")(app);
 
-// Start he API retrieval scheduler
-const apiScheduler = require("./utility/api_scheduler");
-
-console.log("ENV: "+process.env.APISCHEDULER)
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-  }
 });
 
 
